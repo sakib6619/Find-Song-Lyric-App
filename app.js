4cd57a0aa2277e1fcb7ccc3fad9ec5725c2244e2
Shahadot Hossain <79484476+sakib6619@users.noreply.github.com>
2023-12-17 22:55:17 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+document.getElementById('search-btn').addEventListener('click', function () {
+    const inputSearch = document.getElementById('input-Search').value;
+    const url = `https://api.lyrics.ovh/suggest/${inputSearch}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => searchSong(data.data));
+    // .catch(error => displayError('Something is wrong..! try again'));
+})
+
+function searchSong(songs) {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9"> 
+        <h3 class="lyrics-name">${song.title}</h3>
+        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        <audio controls>
+                <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-primary">Get Lyrics</button>
+        </div>
+        `;
+        songContainer.appendChild(songDiv);
+    });
+}
+// get lyrics code 
+
+const getLyrics = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    // try {
+    //     const res = await fetch(url);
+    //     const data = await res.json();
+    //     displayLyrics(data.lyrics);
+    // }
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => displayLyrics(data.lyrics))
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = lyrics;
+}
